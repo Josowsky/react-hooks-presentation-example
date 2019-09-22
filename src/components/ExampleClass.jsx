09ef9347d902b3e766a14f2829bb96d46c16d12b
Josowsky <bartosz@jozwowiak.pl>
2019-09-22 15:36:09 +0200

    Add counter to class example

--- src/components/ExampleClass.jsx
+++ src/components/ExampleClass.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
 
 class ExampleClass extends Component {
+  state = {
+    counter: 0
+  };
+
+  componentDidMount() {
+    const { counter } = this.state;
+
+    document.title = `You clicked ${counter} times`;
+  }
+
+  componentDidUpdate() {
+    const { counter } = this.state;
+
+    document.title = `You clicked ${counter} times`;
+  }
+
+  handleButtonClick = () => {
+    this.setState(({ counter }) => ({
+      counter: counter + 1
+    }));
+  };
+
   render() {
+    const { counter } = this.state;
+
     return (
-      <div>
-        <h1>test</h1>
-      </div>
+      <main>
+        <div>
+          <button onClick={this.handleButtonClick}>Click me ({counter})</button>
+        </div>
+      </main>
     );
   }
 }
