92fe0fb531111b68d48bed69db7929518fb9be40
Josowsky <bartosz@jozwowiak.pl>
2019-09-22 15:12:07 +0200

    Add basic example components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Example from "./components/ExampleClass";
+// import Example from "./components/ExampleHooks";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Example />
       </header>
     </div>
   );
---
+++ src/components/ExampleHooks.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ExampleHooks = () => (
+  <div>
+    <h1>test</h1>
+  </div>
+);
+
+export default ExampleHooks;
