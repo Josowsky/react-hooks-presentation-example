6cdd58326b321c47d64202e516ad02916fcee99a
Josowsky <bartosz@jozwowiak.pl>
2019-09-23 01:48:24 +0200

    Remove logic from class and hooks example

--- src/components/ExampleClass.jsx
+++ src/components/ExampleClass.jsx
@@ -3,51 +3,13 @@ import React, { Component } from "react";
 import { StyledRow, StyledButton } from "./Example.styles";
 
 class ExampleClass extends Component {
-  state = {
-    counter: 0,
-    width: window.innerWidth
-  };
-
-  componentDidMount() {
-    const { counter } = this.state;
-
-    document.title = `You clicked ${counter} times`;
-    window.addEventListener("resize", this.handleWindowResize);
-  }
-
-  componentDidUpdate() {
-    const { counter } = this.state;
-
-    document.title = `You clicked ${counter} times`;
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.handleWindowResize);
-  }
-
-  handleWindowResize = () => {
-    this.setState({
-      width: window.innerWidth
-    });
-  };
-
-  handleButtonClick = () => {
-    this.setState(({ counter }) => ({
-      counter: counter + 1
-    }));
-  };
-
   render() {
-    const { counter, width } = this.state;
-
     return (
       <main>
         <StyledRow>
-          <StyledButton onClick={this.handleButtonClick}>
-            Click me ({counter})
-          </StyledButton>
+          <StyledButton>Click me ()</StyledButton>
         </StyledRow>
-        <StyledRow>Width: {width}</StyledRow>
+        <StyledRow>Width: </StyledRow>
       </main>
     );
   }
--- src/components/ExampleHooks.jsx
+++ src/components/ExampleHooks.jsx
@@ -1,33 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import { StyledRow, StyledButton } from "./Example.styles";
 
 const ExampleHooks = () => {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
-
-  const [width, setWith] = useState(window.innerWidth);
-  useEffect(() => {
-    const handleWindowResize = () => setWith(window.innerWidth);
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => window.removeEventListener("resize", handleWindowResize);
-  });
-
-  const handleButtonClick = () => {
-    setCount(prevCount => prevCount + 1);
-  };
-
   return (
     <main>
       <StyledRow>
-        <StyledButton onClick={handleButtonClick}>
-          Click me ({count})
-        </StyledButton>
+        <StyledButton>Click me ()</StyledButton>
       </StyledRow>
-      <StyledRow>Width {width}</StyledRow>
+      <StyledRow>Width </StyledRow>
     </main>
   );
 };
