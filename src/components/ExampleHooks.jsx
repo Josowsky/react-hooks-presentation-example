0d6a1e069d2d306f4f15b830ba107f758f6d5ebb
dariaoldenburg <dariaoldenburg@gmail.com>
2019-09-22 15:49:49 +0200

    refactor(ExampleHooks)

--- src/components/ExampleHooks.jsx
+++ src/components/ExampleHooks.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 
 const ExampleHooks = () => {
   const [count, setCount] = useState(0);
@@ -7,13 +7,17 @@ const ExampleHooks = () => {
     document.title = `You clicked ${count} times`;
   });
 
+  const handleButtonClick = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
   return (
     <div>
-    <div>
-      <button onClick={() => setCount(count + 1)}>Click me ({count})</button>
+      <div>
+        <button onClick={handleButtonClick}>Click me ({count})</button>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default ExampleHooks;
