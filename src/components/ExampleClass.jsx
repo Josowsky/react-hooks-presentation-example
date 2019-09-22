f3e68ba26218c0dc93e613902e5167e7faf443b4
dariaoldenburg <dariaoldenburg@gmail.com>
2019-09-22 16:15:39 +0200

    feat(ExampleClass): add width

--- src/components/ExampleClass.jsx
+++ src/components/ExampleClass.jsx
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class ExampleClass extends Component {
   state = {
-    counter: 0
+    counter: 0,
+    width: window.innerWidth
   };
 
   componentDidMount() {
     const { counter } = this.state;
 
     document.title = `You clicked ${counter} times`;
+    window.addEventListener('resize', this.handleWindowResize);
   }
 
   componentDidUpdate() {
@@ -17,6 +19,16 @@ class ExampleClass extends Component {
     document.title = `You clicked ${counter} times`;
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize);
+  }
+
+  handleWindowResize = () => {
+    this.setState({
+      width: window.innerWidth
+    });
+  };
+
   handleButtonClick = () => {
     this.setState(({ counter }) => ({
       counter: counter + 1
@@ -24,13 +36,14 @@ class ExampleClass extends Component {
   };
 
   render() {
-    const { counter } = this.state;
+    const { counter, width } = this.state;
 
     return (
       <main>
         <div>
           <button onClick={this.handleButtonClick}>Click me ({counter})</button>
         </div>
+        <div>Width: {width}</div>
       </main>
     );
   }
